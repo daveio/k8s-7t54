edf5aa4c5009ddaf28a51ae87a842d25c3a06ca6
Dave Williams <dave@dave.io>
2024-09-13 23:07:25 +0100

    inject linkerd into gloo

--- bin/004-gloo.sh
+++ bin/004-gloo.sh
@@ -8,3 +8,5 @@ curl -sL https://run.solo.io/gloo/install | sh
 echo "Installing Gloo Gateway"
 
 glooctl install gateway
+
+kubectl get deploy -n gloo-system -o yaml | linkerd inject - | kubectl apply -f-
