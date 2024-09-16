70c7a9adf71c39ecd2d135dc6b0b1668e535a592
Dave Williams <dave@dave.io>
2024-09-16 10:38:48 +0100

    blocky container

--- bin/006-nfs.sh
+++ bin/006-nfs.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
 
-helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner --force-update
+# DISABLED UNTIL WE FIGURE OUT WHAT IS BUSTED
 
-helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
-  --create-namespace \
-  --namespace nfs-provisioner \
-  --values $(dirname $0)/../helm/nfs/values.yaml
+# helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner --force-update
+
+# helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+#   --create-namespace \
+#   --namespace nfs-provisioner \
+#   --values $(dirname $0)/../helm/nfs/values.yaml
